d8e9136303977571a909bc32bd0f782ffb86a33f
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-09-05 14:43:04 +0430

    Replace old implementations

---
+++ src/_old/______.ts
@@ -0,0 +1,30 @@
+import Validation from '../Validation';
+import ValidatorAsync from '../interfaces/ValidatorAsync';
+
+class XValidation extends Validation<'', { a: { b: number; c: ['s'] } }> {
+  constructor() {
+    super(validator => {
+      // const chain = validator.with((0 as any) as [number, boolean, string]).do((x, y, z) => true);
+      // const chain = validator.with((0 as any) as boolean[]).each((b, i, bb) => validator.with(Promise.resolve(true)));
+      // const chain = validator.with((0 as any) as boolean).after(true);
+      // const chain = validator.with((0 as any) as number[]).use(validator.$.a.b, (x, y) => Promise.resolve(''));
+      // const chain = validator.after(7, Promise.resolve(true)).after(([n, b]) => (5).toString(), false, 5, 55);
+      const chain = validator
+        .start('lsfdkfj', 5, 4, true)
+        .with(5)
+        .then(x => Promise.resolve(x))
+        .end();
+    });
+  }
+}
+
+/////////////////////////////////////////////////////////
+
+interface I {
+  /** hi! */
+  new (a: number);
+}
+
+class C implements I {
+  constructor(a: number) {}
+}
