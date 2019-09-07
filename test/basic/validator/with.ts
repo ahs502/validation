1fd9091647fd7c5fab186bd5ca947bf60f5502bc
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-09-07 22:38:26 +0430

    Add tests for with ring

---
+++ test/basic/validator/with.ts
@@ -0,0 +1,94 @@
+import Validation from '../../../src/Validation';
+
+describe('Validator', () => {
+  describe('with ring', () => {
+    it('should work synchronously and provide data', () => {
+      let result: any = undefined;
+      class WithValidation extends Validation<'A'> {
+        constructor(data: any) {
+          super(validator => (result = validator.with(data).earn('A').value));
+        }
+      }
+
+      const data: any = { a: 1 };
+      const validation = new WithValidation(data);
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual(['A']);
+      expect(result).toBe(data);
+    });
+
+    it('should work synchronously and provide data with promises', async () => {
+      let result: any = undefined;
+      class WithValidation extends Validation<'A'> {
+        constructor(data: any) {
+          super(validator =>
+            validator
+              .with(Promise.resolve(data))
+              .earn('A')
+              .value.then(feed => (result = feed))
+          );
+        }
+      }
+
+      const data: any = { a: 1 };
+      const validation = new WithValidation(data);
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual([]);
+      expect(result).toBe(undefined);
+      await validation.async;
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual(['A']);
+      expect(result).toBe(data);
+    });
+
+    it('should work asynchronously and provide data', async () => {
+      let result: any = undefined;
+      class WithValidation extends Validation<'A'> {
+        constructor(data: any) {
+          super(validator =>
+            validator
+              .then(() => Promise.resolve())
+              .with(data)
+              .earn('A')
+              .value.then(feed => (result = feed))
+          );
+        }
+      }
+
+      const data: any = { a: 1 };
+      const validation = new WithValidation(data);
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual([]);
+      expect(result).toBe(undefined);
+      await validation.async;
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual(['A']);
+      expect(result).toBe(data);
+    });
+
+    it('should work asynchronously and provide data with promises', async () => {
+      let result: any = undefined;
+      class WithValidation extends Validation<'A'> {
+        constructor(data: any) {
+          super(validator =>
+            validator
+              .then(() => Promise.resolve())
+              .with(Promise.resolve(data))
+              .earn('A')
+              .value.then(feed => (result = feed))
+          );
+        }
+      }
+
+      const data: any = { a: 1 };
+      const validation = new WithValidation(data);
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual([]);
+      expect(result).toBe(undefined);
+      await validation.async;
+      expect(validation.ok).toBe(true);
+      expect(validation.badges).toEqual(['A']);
+      expect(result).toBe(data);
+    });
+  });
+});
