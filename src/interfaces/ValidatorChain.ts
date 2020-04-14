2fe97f2a3682e5dcb0e7ee4d132e3e4af7eba619
Hessamoddin A Shokravi <ahs502@gmail.com>
2020-04-14 14:55:27 +0430

    Document start and end methods of Validator

--- src/interfaces/ValidatorChain.ts
+++ src/interfaces/ValidatorChain.ts
@@ -11,39 +11,27 @@ export default interface ValidatorChain<Badge extends string, $ extends $Base, D
   do<T = Something | Promise<Something> | Validator<Badge, $, Something>>(
     task: Data extends readonly []
       ? () => T
-      : Data extends readonly [(infer D0)]
+      : Data extends readonly [infer D0]
       ? (d0: D0) => T
-      : Data extends readonly [(infer D0), (infer D1)]
+      : Data extends readonly [infer D0, infer D1]
       ? (d0: D0, d1: D1) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2)]
+      : Data extends readonly [infer D0, infer D1, infer D2]
       ? (d0: D0, d1: D1, d2: D2) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3]
       ? (d0: D0, d1: D1, d2: D2, d3: D3) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4), (infer D5)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4), (infer D5), (infer D6)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5, infer D6]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5, d6: D6) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4), (infer D5), (infer D6), (infer D7)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5, infer D6, infer D7]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5, d6: D6, d7: D7) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4), (infer D5), (infer D6), (infer D7), (infer D8)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5, infer D6, infer D7, infer D8]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5, d6: D6, d7: D7, d8: D8) => T
-      : Data extends readonly [(infer D0), (infer D1), (infer D2), (infer D3), (infer D4), (infer D5), (infer D6), (infer D7), (infer D8), (infer D9)]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5, infer D6, infer D7, infer D8, infer D9]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5, d6: D6, d7: D7, d8: D8, d9: D9) => T
-      : Data extends readonly [
-          (infer D0),
-          (infer D1),
-          (infer D2),
-          (infer D3),
-          (infer D4),
-          (infer D5),
-          (infer D6),
-          (infer D7),
-          (infer D8),
-          (infer D9),
-          ...(infer I)[]
-        ]
+      : Data extends readonly [infer D0, infer D1, infer D2, infer D3, infer D4, infer D5, infer D6, infer D7, infer D8, infer D9, ...(infer I)[]]
       ? (d0: D0, d1: D1, d2: D2, d3: D3, d4: D4, d5: D5, d6: D6, d7: D7, d8: D8, d9: D9, ...others: I[]) => T
       : never
   ): Data extends readonly any[] ? ValidatorChain<Badge, $, ResultValidator<Badge, $, T>> : never;
@@ -317,7 +305,7 @@ export default interface ValidatorChain<Badge extends string, $ extends $Base, D
 
   fail(badge: Badge, message?: string | Provider<Data, string>): ValidatorChain<Badge, $, Data>;
 
-  set: Data extends undefined ? (() => never) : (($path: Data) => ValidatorChain<Badge, $, Data>);
+  set: Data extends undefined ? () => never : ($path: Data) => ValidatorChain<Badge, $, Data>;
 
   put<T = Something, U = T | Promise<T>>($path: T, value: U | Provider<Data, U>): ValidatorChain<Badge, $, ResultPromise<U>>;
 
@@ -325,13 +313,70 @@ export default interface ValidatorChain<Badge extends string, $ extends $Base, D
 
   use<T = Something, U = Something | Promise<Something> | Validator<Badge, $, Something>>(
     $path: T,
-    task: Data extends undefined ? ((value: T) => U) : ((value: T, data: Data) => U)
+    task: Data extends undefined ? (value: T) => U : (value: T, data: Data) => U
   ): ValidatorChain<Badge, $, ResultValidator<Badge, $, U>>;
 
   readonly value: Data | Promise<Data>;
 
   /**
-   * TODO
+   * Closes a named *chain* and caches its results to be used later in continuous validation.
+   *
+   * It should be the last *ring* in a *chain* and that *chain* must begin with a `stort` *ring* method.
+   *
+   * --------------------------------
+   * Example:
+   *
+   *```typescript
+   *interface User {
+   *  firstName: string;
+   *  lastName: string;
+   *  age: number;
+   *}
+   *
+   *class UserValidation extends Validation {
+   *  constructor({ firstName, lastName, age }: User, previousUserValidation?: UserValidation) {
+   *    super(
+   *      validator => {
+   *
+   *        validator
+   *          .start('name', firstName, lastName)
+   *          ...
+   *          .check( ... ) // Some heavy/async check on name
+   *          ...
+   *          .end();
+   *
+   *        validator
+   *          .start('age', age)
+   *          ...
+   *          .check( ... ) // Some heavy/async check on age
+   *          ...
+   *          .end();
+   *
+   *      },
+   *      previousUserValidation
+   *    );
+   *  }
+   *}
+   *
+   *let validation: userValidation | undefined = undefined;
+   *
+   *do {
+   *  const user = getUser();
+   *
+   *  // This is called continuous validation; to feed the previous validation
+   *  // to the new one to achieve more performance.
+   *  // While either user.firstName or user.lastName don't change, the checks for them in
+   *  // the 'name' named chain won't happen again and the result will be copied from before.
+   *  // The same happens for user.age and 'age' named chain.
+   *  validation = new UserValidation(user, validation);
+   *
+   *  if (validation.ok) {
+   *    // The user data is valid.
+   *  }
+   *} while (...)
+   *```
+   * --------------------------------
+   * @see `start` *ring* method.
    */
   end(): Data | Promise<Data>;
 }
--- src/interfaces/ValidatorSeed.ts
+++ src/interfaces/ValidatorSeed.ts
@@ -4,12 +4,109 @@ import ValidatorChain from './ValidatorChain';
 
 export default interface ValidatorSeed<Badge extends string, $ extends $Base> extends Validator<Badge, $, undefined> {
   /**
-   * TODO
+   * The validation internal storage, mostly used for nested validation.
+   *
+   * During validation, it is accessible through `set`, `put`, `get` and `use` *ring* methods.
+   *
+   * After validation, it is accessible through `validation.$` property.
+   *
+   * --------------------------------
+   * Example:
+   *
+   *```typescript
+   *class MyValidation extends Validation<string, {
+   *  alpha: {
+   *    beta: number[]
+   *  }
+   *}> {
+   *  constructor() {
+   *    super(validator => {
+   *      validator
+   *        .put(validator.$.alpha.beta[0], 10);
+   *
+   *      validator
+   *        .use(validator.$.alpha.beta[0], data => data + 5)
+   *        .then(console.log);
+   *    })
+   *  }
+   *}
+   *
+   *const myValidation = new MyValidation() // Prints 15
+   *
+   *myValidation.$.alpha.beta[0] // Equals to 10
+   *```
+   * --------------------------------
+   * @see `set` *ring* method.
+   * @see `put` *ring* method.
+   * @see `get` *ring* method.
+   * @see `use` *ring* method.
    */
   readonly $: $;
 
   /**
-   * TODO
+   * Names a *chain* and skips unnecessary checks in continuous validation.
+   *
+   * It should be the first *ring* in a *chain* and that *chain* must end with an `end` *ring* method.
+   * Otherwise, the *chain* activity won't be cached and will be uneffective.
+   *
+   * Named *chain*s can not be introduced within other named *chain*s.
+   *
+   * --------------------------------
+   * Example:
+   *
+   *```typescript
+   *interface User {
+   *  firstName: string;
+   *  lastName: string;
+   *  age: number;
+   *}
+   *
+   *class UserValidation extends Validation {
+   *  constructor({ firstName, lastName, age }: User, previousUserValidation?: UserValidation) {
+   *    super(
+   *      validator => {
+   *
+   *        validator
+   *          .start('name', firstName, lastName)
+   *          ...
+   *          .check( ... ) // Some heavy/async check on name
+   *          ...
+   *          .end();
+   *
+   *        validator
+   *          .start('age', age)
+   *          ...
+   *          .check( ... ) // Some heavy/async check on age
+   *          ...
+   *          .end();
+   *
+   *      },
+   *      previousUserValidation
+   *    );
+   *  }
+   *}
+   *
+   *let validation: userValidation | undefined = undefined;
+   *
+   *do {
+   *  const user = getUser();
+   *
+   *  // This is called continuous validation; to feed the previous validation
+   *  // to the new one to achieve more performance.
+   *  // While either user.firstName or user.lastName don't change, the checks for them in
+   *  // the 'name' named chain won't happen again and the result will be copied from before.
+   *  // The same happens for user.age and 'age' named chain.
+   *  validation = new UserValidation(user, validation);
+   *
+   *  if (validation.ok) {
+   *    // The user data is valid.
+   *  }
+   *} while (...)
+   *```
+   * --------------------------------
+   * @param name The unique name of the *chain*, shouldn't be duplicated.
+   * @param watches Optional, the list of all *chain* dependencies. The *chain* won't run again next time until one of these dependencies changes.
+   * @see `end` *ring* method.
    */
   start(name: string, ...watches: any[]): ValidatorChain<Badge, $, undefined>;
 }
